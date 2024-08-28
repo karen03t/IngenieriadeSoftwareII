133567ab91b77fe90dfbe59cc3af5ea23ea67184
Karen Tatiana Orrego <karentatianaorrego3@gmail.com>
2024-08-28 14:22:04 -0500

    taller 4 de OCP sobre impuesto

---
+++ Taller4/src/taller4/Calculo.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller4;
+
+/**
+ *
+ * @author karen
+ */
+public abstract class Calculo {
+    private String producto;
+    private int cantidad;
+    private Double precio;
+    
+    
+    public abstract double calcularImpuesto();
+    
+    public Calculo(String producto, int cantidad, Double precio) {
+        this.producto = producto;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
+    
+    
+
+    /**
+     * @return the producto
+     */
+    public String getProducto() {
+        return producto;
+    }
+
+    /**
+     * @param producto the producto to set
+     */
+    public void setProducto(String producto) {
+        this.producto = producto;
+    }
+
+    /**
+     * @return the cantidad
+     */
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    /**
+     * @param cantidad the cantidad to set
+     */
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    /**
+     * @return the precio
+     */
+    public Double getPrecio() {
+        return precio;
+    }
+
+    /**
+     * @param precio the precio to set
+     */
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+    
+    
+}
---
+++ Taller4/src/taller4/Factura.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller4;
+
+/**
+ *
+ * @author karen
+ */
+public class Factura {
+    
+    public double calcularImpuesto(Calculo calculo){
+        return calculo.calcularImpuesto();
+        
+    }
+}
